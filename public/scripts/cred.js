2b7a8bbbcfdad084eb5bc3d10ea43ea47a9d75a7
= <=>
2020-06-26 20:30:30 +0530

    Blockchain connected

---
+++ public/scripts/cred.js
@@ -0,0 +1,182 @@
+var ABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_billno",
+				"type": "int256"
+			},
+			{
+				"name": "_gstno",
+				"type": "string"
+			},
+			{
+				"name": "_phoneno",
+				"type": "string"
+			},
+			{
+				"name": "_billamnt",
+				"type": "int256"
+			},
+			{
+				"name": "_gstamnt",
+				"type": "int256"
+			}
+		],
+		"name": "addbilldetails",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_gstin",
+				"type": "string"
+			},
+			{
+				"name": "_businessname",
+				"type": "string"
+			},
+			{
+				"name": "_aadharno",
+				"type": "string"
+			},
+			{
+				"name": "_phoneno",
+				"type": "string"
+			},
+			{
+				"name": "_password",
+				"type": "string"
+			},
+			{
+				"name": "_confirmpass",
+				"type": "string"
+			}
+		],
+		"name": "addBusiness",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_gstin",
+				"type": "string"
+			},
+			{
+				"name": "_password",
+				"type": "string"
+			}
+		],
+		"name": "businessLogin",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getBusinessDetails",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "gstbilldetails",
+		"outputs": [
+			{
+				"name": "",
+				"type": "int256"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "int256"
+			},
+			{
+				"name": "",
+				"type": "int256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_billno",
+				"type": "int256"
+			}
+		],
+		"name": "searchBill",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "int256"
+			},
+			{
+				"name": "",
+				"type": "int256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+var address = "0x2F57Ce3dD00Ac136f94bD5Ba6f02450dC4013F07";
