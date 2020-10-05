1c47441c31dcf0f725ce6237302fabda19cdb35e
= <=>
2020-10-05 15:35:40 +0530

    new version

--- public/scripts/cred.js
+++ public/scripts/cred.js
@@ -1,119 +1,5 @@
 var ABI = [
 	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "_billno",
-				"type": "int256"
-			},
-			{
-				"name": "_gstno",
-				"type": "string"
-			},
-			{
-				"name": "_aadhar",
-				"type": "string"
-			},
-			{
-				"name": "_phoneno",
-				"type": "string"
-			},
-			{
-				"name": "_billamnt",
-				"type": "int256"
-			},
-			{
-				"name": "_gstamnt",
-				"type": "int256"
-			}
-		],
-		"name": "addbilldetails",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "_accno",
-				"type": "string"
-			},
-			{
-				"name": "_gstin",
-				"type": "string"
-			},
-			{
-				"name": "_businessname",
-				"type": "string"
-			},
-			{
-				"name": "_aadharno",
-				"type": "string"
-			},
-			{
-				"name": "_phoneno",
-				"type": "string"
-			},
-			{
-				"name": "_password",
-				"type": "string"
-			},
-			{
-				"name": "_accbal",
-				"type": "int256"
-			}
-		],
-		"name": "addBusiness",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"name": "_amount",
-				"type": "int256"
-			},
-			{
-				"name": "username",
-				"type": "string"
-			},
-			{
-				"name": "password",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "_gstin",
-				"type": "string"
-			},
-			{
-				"name": "_password",
-				"type": "string"
-			}
-		],
-		"name": "businessLogin",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
 		"constant": true,
 		"inputs": [
 			{
@@ -166,24 +52,6 @@ var ABI = [
 	},
 	{
 		"constant": true,
-		"inputs": [],
-		"name": "getGovt",
-		"outputs": [
-			{
-				"name": "",
-				"type": "int256"
-			},
-			{
-				"name": "",
-				"type": "int256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
 		"inputs": [
 			{
 				"name": "_gvtuser",
@@ -195,6 +63,101 @@ var ABI = [
 			}
 		],
 		"name": "governmentLogin",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_accno",
+				"type": "string"
+			},
+			{
+				"name": "_gstin",
+				"type": "string"
+			},
+			{
+				"name": "_businessname",
+				"type": "string"
+			},
+			{
+				"name": "_aadharno",
+				"type": "string"
+			},
+			{
+				"name": "_phoneno",
+				"type": "string"
+			},
+			{
+				"name": "_password",
+				"type": "string"
+			},
+			{
+				"name": "_accbal",
+				"type": "int256"
+			}
+		],
+		"name": "addBusiness",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_billno",
+				"type": "int256"
+			},
+			{
+				"name": "_gstno",
+				"type": "string"
+			},
+			{
+				"name": "_aadhar",
+				"type": "string"
+			},
+			{
+				"name": "_phoneno",
+				"type": "string"
+			},
+			{
+				"name": "_billamnt",
+				"type": "int256"
+			},
+			{
+				"name": "_gstamnt",
+				"type": "int256"
+			}
+		],
+		"name": "addbilldetails",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_gstin",
+				"type": "string"
+			},
+			{
+				"name": "_password",
+				"type": "string"
+			}
+		],
+		"name": "businessLogin",
 		"outputs": [
 			{
 				"name": "",
@@ -243,7 +206,44 @@ var ABI = [
 		"payable": false,
 		"stateMutability": "view",
 		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getGovt",
+		"outputs": [
+			{
+				"name": "",
+				"type": "int256"
+			},
+			{
+				"name": "",
+				"type": "int256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_amount",
+				"type": "int256"
+			},
+			{
+				"name": "username",
+				"type": "string"
+			},
+			{
+				"name": "password",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
 	}
 ];
 
-const address = "0x41c907d4D17170B03Ee6c5ec443E0B3203e513b7";
+const address = "0xA3D48975FD6BD718796813E97891575EADce544a";
